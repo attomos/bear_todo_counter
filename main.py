c8174583bba00f73342e0383742958ce2435fb32
Nattaphoom Chaipreecha <attomos@gmail.com>
2019-07-27 17:54:59 +0700

    Format all Python files using black

--- main.py
+++ main.py
@@ -7,34 +7,36 @@ def append_todo_count(txt):
     complete = 0
     total = 0
     for line in lines:
-        if re.match(r'^\s*\+\s+\[?\S*', line):
+        if re.match(r"^\s*\+\s+\[?\S*", line):
             complete += 1
-        if re.match(r'^\s*[\+|-]\s+\[?\S*', line):
+        if re.match(r"^\s*[\+|-]\s+\[?\S*", line):
             total += 1
-    todo_count = '[{}/{}]'.format(complete, total)
+    todo_count = "[{}/{}]".format(complete, total)
 
-    total_pattern = r'\[\d+/\d+\]'
+    total_pattern = r"\[\d+/\d+\]"
     if re.search(total_pattern, lines[0]):
         lines[0] = re.sub(total_pattern, todo_count, lines[0])
     else:
-        lines[0] += ' {}'.format(todo_count)
-    return '\n'.join(lines)
+        lines[0] += " {}".format(todo_count)
+    return "\n".join(lines)
 
 
 # https://stackoverflow.com/a/25802742/606355
 def write_to_clipboard(output):
     process = subprocess.Popen(
-        'pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=subprocess.PIPE)
-    process.communicate(output.encode('utf-8'))
+        "pbcopy", env={"LANG": "en_US.UTF-8"}, stdin=subprocess.PIPE
+    )
+    process.communicate(output.encode("utf-8"))
 
 
 def read_from_clipboard():
-    return subprocess.check_output(
-        'pbpaste', env={'LANG': 'en_US.UTF-8'}).decode('utf-8')
+    return subprocess.check_output("pbpaste", env={"LANG": "en_US.UTF-8"}).decode(
+        "utf-8"
+    )
 
 
 if __name__ == "__main__":
     txt = read_from_clipboard()
     output = append_todo_count(txt)
-    print(output.split('\n')[0])
+    print(output.split("\n")[0])
     write_to_clipboard(output)
